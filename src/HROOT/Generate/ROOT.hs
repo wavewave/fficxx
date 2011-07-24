35ff75ab6408a1f6ef657842d4c14166084c6232
ianwookim <ianwookim@gmail.com>
2011-07-24 02:07:02 +0000

    add a bunch of classes in ROOT.hs

--- src/HROOT/Generate/ROOT.hs
+++ src/HROOT/Generate/ROOT.hs
@@ -55,30 +55,6 @@ tAttAxis = Class "TAttAxis" []
                  , Function void_   "SetNdivisions"   [int "n", bool "optim" ] Ordinary
                  ] 
  
-tH1 :: Class
-tH1 = Class "TH1" [tNamed, tAttLine, tAttFill, tAttMarker] 
-      [ Function (cppclass "TAxis") "GetXaxis" [] Ordinary
-      , Function (cppclass "TAxis") "GetYaxis" [] Ordinary
-      , Function (cppclass "TAxis") "GetZaxis" [] Ordinary
-      , Function void_ "Add" [ (CPT (CPTClass "TH1") NoConst, "h1"), double "c1" ] Ordinary 
-      , Function int_  "Fill" [double "x"] (Alias "fill1")
-      ] 
-
-tH1F :: Class
-tH1F = Class "TH1F" [tH1] 
-       [ Function self_ "New" [cstring "name",cstring "title",int "nbinsx",double "xlow",double "xup"] Constructor
-       ] 
-           
-tH2 :: Class 
-tH2 = Class "TH2" [tH1] 
-      [ Function int_ "Fill" [double "x", double "y"] (Alias "fill2") 
-      ]
-
-tH2F :: Class
-tH2F = Class "TH2F" [tH1] 
-       [ Function self_ "New" [cstring "name",cstring "title",int "nbinsx",double "xlow",double "xup"
-                              ,int "nbinsy", double "ylow", double "yup"] Constructor
-       ]
 
 tHStack :: Class
 tHStack = Class "THStack" [tNamed] 
@@ -160,13 +136,172 @@ tFile = Class "TFile" [tDirectoryFile]
         [ Function self_ "New" [cstring "fname", cstring "option", cstring "ftitle", int "compress" ] Constructor
         ]
 
+tTree :: Class 
+tTree = Class "TTree" [tNamed, tAttLine, tAttFill, tAttMarker]
+        []
+
+tBranch :: Class
+tBranch = Class "TBranch" [tNamed, tAttFill]
+          []
+
+tVirtualTreePlayer :: Class
+tVirtualTreePlayer = Class "TVirtualTreePlayer" [tObject]
+                     []
+
+tTreePlayer :: Class
+tTreePlayer = Class "TTreePlayer" [tVirtualTreePlayer] 
+              []
+                 
+
+tAtt3D :: Class
+tAtt3D = Class "TAtt3D" []
+         []
+
+
+tArray :: Class
+tArray = Class "TArray" [] 
+         []
+
+tArrayC :: Class 
+tArrayC = Class "TArrayC" [tArray]
+          []
+
+tArrayD :: Class
+tArrayD = Class "TArrayD" [tArray]
+          []
+
+tArrayF :: Class 
+tArrayF = Class "TArrayF" [tArray]
+          []
+
+tArrayI :: Class
+tArrayI = Class "TArrayI" [tArray]
+          []
+
+tArrayL :: Class
+tArrayL = Class "TArrayL" [tArray]
+          []
+
+tArrayL64 :: Class
+tArrayL64 = Class "TArrayL64" [tArray]
+            []
+
+tArrayS :: Class
+tArrayS = Class "TArrayS" [tArray]
+          []
+
+tH1 :: Class
+tH1 = Class "TH1" [tNamed, tAttLine, tAttFill, tAttMarker] 
+      [ Function (cppclass "TAxis") "GetXaxis" [] Ordinary
+      , Function (cppclass "TAxis") "GetYaxis" [] Ordinary
+      , Function (cppclass "TAxis") "GetZaxis" [] Ordinary
+      , Function void_ "Add" [ (CPT (CPTClass "TH1") NoConst, "h1"), double "c1" ] Ordinary 
+      , Function int_  "Fill" [double "x"] (Alias "fill1")
+      ] 
+
+tH2 :: Class 
+tH2 = Class "TH2" [tH1] 
+      [ Function int_ "Fill" [double "x", double "y"] (Alias "fill2") 
+      ]
+
+
+tH3 :: Class
+tH3 = Class "TH3" [tH1, tAtt3D]
+      []
+
+
+tH1C :: Class 
+tH1C = Class "TH1C" [tH1, tArrayC]
+       []
+ 
+tH1D :: Class
+tH1D = Class "TH1D" [tH1, tArrayD]
+       []
+
+tH1F :: Class
+tH1F = Class "TH1F" [tH1, tArrayF] 
+       [ Function self_ "New" [cstring "name",cstring "title",int "nbinsx",double "xlow",double "xup"] Constructor
+       ] 
+           
+tH1I :: Class 
+tH1I = Class "TH1I" [tH1, tArrayI]
+       []
+
+tH1K :: Class
+tH1K = Class "TH1K" [tH1, tArrayF]
+       []
+
+tH1S :: Class
+tH1S = Class "TH1S" [tH1, tArrayS]
+       []
+
+tH2C :: Class
+tH2C = Class "TH2C" [tH2, tArrayC]
+       []
+
+tH2D :: Class 
+tH2D = Class "TH2D" [tH2, tArrayD] 
+       []
+
+tH2F :: Class
+tH2F = Class "TH2F" [tH2, tArrayF] 
+       [ Function self_ "New" [cstring "name",cstring "title",int "nbinsx",double "xlow",double "xup"
+                              ,int "nbinsy", double "ylow", double "yup"] Constructor
+       ]
+
+tH2I :: Class
+tH2I = Class "TH2I" [tH2, tArrayI]
+       []
+
+tH2Poly :: Class 
+tH2Poly = Class "TH2Poly" [tH2]
+          []
+
+tH2S :: Class
+tH2S = Class "TH2S" [tH2, tArrayS]
+       []
+
+
+
+tH3C :: Class 
+tH3C = Class "TH3C" [tH3, tArrayC]
+       []
+
+tH3D :: Class
+tH3D = Class "TH3D" [tH3, tArrayD] 
+       []
+
+tH3F :: Class
+tH3F = Class "TH3F" [tH3, tArrayF]
+       []
+
+tH3I :: Class
+tH3I = Class "TH3I" [tH3, tArrayI]
+       []
+
+tH3S :: Class
+tH3S = Class "TH3S" [tH3, tArrayS]
+       []
+
+
 root_all_classes :: [Class]
-root_all_classes = [ tObject, tNamed, tFormula, tAttLine, tAttFill, tWbox, tAttAxis
-                   , tAttText, tH1F, tH2F, tHStack, tCanvas, tF1, tGraph
-                   , tAxis, tLine, tLatex, tH1, tH2, tApplication, tText
-                   , tDirectory, tDirectoryFile, tFile
-                   , tAttMarker, tAttPad
-                   , tVirtualPad, tPad ]
+root_all_classes = 
+  [ tObject, tNamed, tFormula
+  , tWbox
+  , tAttLine, tAttFill, tAttAxis, tAttText, tAtt3D, tAttMarker
+  , tAttPad
+  , tHStack, tCanvas, tF1, tGraph
+  , tAxis, tLine, tLatex, tApplication, tText
+  , tDirectory, tDirectoryFile, tFile
+  , tVirtualPad, tPad
+  , tTree, tBranch, tVirtualTreePlayer, tTreePlayer
+  , tArray, tArrayC, tArrayD, tArrayF, tArrayI, tArrayL, tArrayL64
+  , tArrayS
+  , tH1, tH2, tH3
+  , tH1C, tH1D, tH1F, tH1I, tH1S
+  , tH2C, tH2D, tH2F, tH2I, tH2Poly, tH2S 
+  , tH3C, tH3D, tH3F, tH3I, tH3S  
+  ]
 
 
 
