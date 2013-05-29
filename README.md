b2ca9d124642395d78ad888d3daa9ad501bb3ff1
Ian-Woo Kim <ianwookim@gmail.com>
2013-05-29 12:07:28 -0400

    working my sample

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 What is fficxx?
 ===============
 
-fficxx is an automatic haskell Foreign Function Interface (FFI) generator to C++. While haskell has a well-specified standard for C FFI, making haskell-C++ FFI support  is an arbitrary and painful process. Since Object-Oriented Programming (OOP) paradigm and Functional Programming (FP) paradigm are different, automatic translation of C++ libraries to haskell libraries is not a straightforward task. The goal of fficxx is to minimize this disparity and maximize user's convenience by providing familiar interface to the original C++ library as a result. 
+fficxx is an automatic haskell Foreign Function Interface (FFI) generator to C++. While haskell has a well-specified standard for C FFI, making haskell-C++ FFI is an arbitrary and painful process. Since Object-Oriented Programming (OOP) paradigm and Functional Programming (FP) paradigm are different, automatic translation of C++ libraries to haskell libraries is not a straightforward task. The goal of fficxx is to minimize this disparity and maximize user's convenience by providing familiar interface to the original C++ library as a result. 
 
-One of the projects that uses fficxx is [HROOT](http://ianwookim.org/HROOT) (haskell binding to the ROOT framework).  [HROOT-generate](http://github.com/wavewave/HROOT-generate) is a haskell script using fficxx that generates HROOT packages. Once generated, each package can be directly installable as a cabal package. Currently, C++ interface is defined as a haskell data structure as one can see, for example, in the module [HROOT.Data.Core.Class](https://github.com/wavewave/HROOT-generate/blob/master/lib/HROOT/Data/Core/Class.hs). At this moment, automatic generation from C++ code is not supported yet, but it is planned to be supported. 
+One of the projects that successfully uses fficxx is [HROOT](http://ianwookim.org/HROOT) which is a haskell binding to the [ROOT](http://root.cern.ch) library. A haskell script called [HROOT-generate](http://github.com/wavewave/HROOT-generate) using fficxx generates HROOT packages. Once generated, each package can be directly installable as a cabal package. Currently, C++ interface is defined as a haskell data structure as one can see, for example, in the module [HROOT.Data.Core.Class](https://github.com/wavewave/HROOT-generate/blob/master/lib/HROOT/Data/Core/Class.hs). At this moment, automatic generation from C++ code is not supported yet, but it is planned to be supported. 
 
 fficxx is separated into generator part and runtime part: 
 
-* fficxx : FFI binding generator library
+* fficxx : FFI types and binding generator library
 * fficxx-runtime : runtime modules needed for various common routines 
 
 Haskell packages that are generated from fficxx will be dependent on fficxx-runtime. 
---
+++ sample/mysample-generator/src/MySampleGen.hs
@@ -0,0 +1,144 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Data.Monoid (mempty)
+import System.FilePath ((</>))
+import System.Directory (getCurrentDirectory)
+import           Text.StringTemplate hiding (render)
+-- 
+import           Bindings.Cxx.Generate.Code.Cabal
+import           Bindings.Cxx.Generate.Code.Cpp
+import           Bindings.Cxx.Generate.Code.Dependency
+import           Bindings.Cxx.Generate.Config
+import           Bindings.Cxx.Generate.Code.Cpp
+import           Bindings.Cxx.Generate.Code.Dependency
+import           Bindings.Cxx.Generate.Config
+import           Bindings.Cxx.Generate.Generator.ContentMaker 
+import           Bindings.Cxx.Generate.Generator.Driver
+import           Bindings.Cxx.Generate.Type.Annotate
+import           Bindings.Cxx.Generate.Type.Class
+import           Bindings.Cxx.Generate.Type.PackageInterface
+import           Bindings.Cxx.Generate.Util
+-- 
+import qualified Paths_fficxx as F
+
+mysampleclasses = [ ] 
+
+mycabal = Cabal { cabal_pkgname = "MySample" 
+                , cabal_cheaderprefix = "MySample"
+                , cabal_moduleprefix = "MySample" }
+
+myclass = Class mycabal 
+
+a :: Class 
+a = myclass "A" [] mempty 
+    [ Constructor [] 
+    , Virtual void_ "Foo" [ ] 
+    ] 
+
+b :: Class 
+b = myclass "B" [a] mempty 
+    [ Constructor [] 
+    , Virtual void_ "Bar" [] 
+    ] 
+
+myclasses = [ a, b] 
+
+
+-- | 
+cabalTemplate :: String 
+cabalTemplate = "Pkg.cabal"
+
+
+-- | 
+mkCabalFile :: FFICXXConfig
+            -> STGroup String  
+            -> [ClassModule]
+            -> FilePath  
+            -> IO () 
+mkCabalFile config templates classmodules cabalfile = do 
+  let str = renderTemplateGroup 
+              templates 
+              [ ("pkgname", "MySample") 
+              , ("version",  "0.0") 
+              , ("license", "" ) 
+              , ("buildtype", "Simple")
+              , ("deps", "" ) 
+              , ("csrcFiles", genCsrcFiles classmodules)
+              , ("includeFiles", genIncludeFiles "MySample" classmodules) 
+              , ("cppFiles", genCppFiles classmodules)
+              , ("exposedModules", genExposedModules "MySample" classmodules) 
+              , ("otherModules", genOtherModules classmodules)
+              , ("cabalIndentation", cabalIndentation)
+              ]
+              cabalTemplate 
+  writeFile cabalfile str
+
+
+
+
+main :: IO ()
+main = do 
+  putStrLn "generate mysample" 
+  cwd <- getCurrentDirectory 
+
+  let cfg =  FFICXXConfig { fficxxconfig_scriptBaseDir = cwd 
+                          , fficxxconfig_workingDir = cwd </> "working"
+                          , fficxxconfig_installBaseDir = cwd </> "MySample"  
+                          } 
+      workingDir = fficxxconfig_workingDir cfg
+      installDir = fficxxconfig_installBaseDir cfg 
+      pkgname = "MySample" 
+      (mods,cihs) = mkAllClassModulesAndCIH ("MySample", (\c->([],[class_name c ++ ".h"]))) myclasses
+      hsbootlst = mkHSBOOTCandidateList mods 
+      cglobal = mkGlobal myclasses 
+      summarymodule = "MySample" 
+      cabalFileName = "MySample.cabal"
+  templateDir <- F.getDataDir >>= return . (</> "template")
+  (templates :: STGroup String) <- directoryGroup templateDir 
+  -- 
+  notExistThenCreate workingDir
+  notExistThenCreate installDir 
+  notExistThenCreate (installDir </> "src") 
+  notExistThenCreate (installDir </> "csrc")
+  -- 
+  putStrLn "cabal file generation" 
+  mkCabalFile cfg templates mods (workingDir </> cabalFileName)
+  -- 
+  putStrLn "header file generation"
+  writeTypeDeclHeaders templates workingDir (TypMcro "__MYSAMPLE__") pkgname cihs
+  mapM_ (writeDeclHeaders templates workingDir (TypMcro "__MYSAMPLE__") pkgname) cihs
+  -- 
+  putStrLn "cpp file generation" 
+  mapM_ (writeCppDef templates workingDir) cihs
+  -- 
+  putStrLn "RawType.hs file generation" 
+  mapM_ (writeRawTypeHs templates workingDir) mods 
+  -- 
+  putStrLn "FFI.hsc file generation"
+  mapM_ (writeFFIHsc templates workingDir) mods
+  -- 
+  putStrLn "Interface.hs file generation" 
+  mapM_ (writeInterfaceHs mempty templates workingDir) mods
+  -- 
+  putStrLn "Cast.hs file generation"
+  mapM_ (writeCastHs templates workingDir) mods
+  -- 
+  putStrLn "Implementation.hs file generation"
+  mapM_ (writeImplementationHs mempty templates workingDir) mods
+  -- 
+  putStrLn "hs-boot file generation" 
+  mapM_ (writeInterfaceHSBOOT templates workingDir) hsbootlst  
+  -- 
+  putStrLn "module file generation" 
+  mapM_ (writeModuleHs templates workingDir) mods
+  -- 
+  putStrLn "summary module generation generation"
+  writePkgHs summarymodule templates workingDir mods
+  -- 
+  putStrLn "copying"
+  copyFileWithMD5Check (workingDir </> cabalFileName)  (installDir </> cabalFileName) 
+  -- copyPredefined templateDir (srcDir ibase) pkgname
+  mapM_ (copyCppFiles workingDir (csrcDir installDir) pkgname) cihs
+  mapM_ (copyModule workingDir (srcDir installDir) summarymodule) mods 
+
+
