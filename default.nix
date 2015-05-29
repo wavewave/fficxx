0ee9f344f56f00f9c788089e9980db0bd30376fa
Ian-Woo Kim <ianwookim@gmail.com>
2015-05-29 10:50:38 +0000

    nixify. add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, base, bytestring, Cabal, containers, directory
+, either, errors, filepath, hashable, HStringTemplate, lens, mtl
+, process, pureMD5, split, stdenv, template-haskell, transformers
+, unordered-containers
+}:
+mkDerivation {
+  pname = "fficxx";
+  version = "0.2.0";
+  src = ./.;
+  buildDepends = [
+    base bytestring Cabal containers directory either errors filepath
+    hashable HStringTemplate lens mtl process pureMD5 split
+    template-haskell transformers unordered-containers
+  ];
+  description = "automatic C++ binding generation";
+  license = stdenv.lib.licenses.bsd3;
+}
