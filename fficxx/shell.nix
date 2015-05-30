95394d892b128a1b98f8406f072fb7ae93e8c895
Ian-Woo Kim <ianwookim@gmail.com>
2015-05-30 09:17:32 +0000

    mv some nix files to fficxx subdirectory

---
+++ fficxx/shell.nix
@@ -0,0 +1,23 @@
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, base, bytestring, Cabal, containers, directory
+             , either, errors, filepath, hashable, HStringTemplate, lens, mtl
+             , process, pureMD5, split, stdenv, template-haskell, transformers
+             , unordered-containers
+             }:
+             mkDerivation {
+               pname = "fficxx";
+               version = "0.2.0";
+               src = ./.;
+               buildDepends = [
+                 base bytestring Cabal containers directory either errors filepath
+                 hashable HStringTemplate lens mtl process pureMD5 split
+                 template-haskell transformers unordered-containers
+		 fficxx-runtime
+               ];
+	       buildTools = [ haskellngPackages.cabal-install pkgs.snappy ];
+               description = "automatic C++ binding generation";
+               license = stdenv.lib.licenses.bsd3;
+             }) {};
+in
+  pkg.env
