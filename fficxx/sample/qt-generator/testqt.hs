9bf52d659b56c2b4b5d85e4413f1aaaa02967c1d
Ian-Woo Kim <ianwookim@gmail.com>
2015-05-31 14:31:24 +0200

    now simplify example

--- fficxx/sample/qt-generator/testqt.hs
+++ fficxx/sample/qt-generator/testqt.hs
@@ -9,39 +9,21 @@ import Foreign.Marshal.Array
 import Foreign.Marshal.Alloc
 import Foreign.C.String
 import Foreign.C.Types
-import Foreign.ForeignPtr
-import Foreign.ForeignPtr.Unsafe
-import Foreign.StablePtr 
 import Foreign.Storable
-
-import System.IO
+import System.Environment
+--
 import Qt5 
 
--- foreign import ccall "test.h set_string" c_set_string :: Ptr CString -> IO ()
-
--- foreign import ccall "test.h get_string" c_get_string :: IO (Ptr ())
 
 main = do 
-    -- args <- getArgs
-    -- argc <-
-    -- alloca $ \argv -> do 
-    -- c_set_string argv
-    -- argv <- c_get_string
-  alloca $ \ptr -> do
-    poke ptr (0 :: CInt)
-    cstr <- newCString "test"
-    argv <- newArray [cstr] 
-    
-    cstrhello <- newCString "Hello world"
-    hFlush stdout
- 
-    putStrLn "point a"
-    -- threadDelay 1000000
-    app <- newQApplication ptr nullPtr --- 1 argv  -- (castPtr (castStablePtrToPtr argv)) -- (argv :: Ptr CString)
-  
-    putStrLn "point b"
-
-    hello <- newQLabel cstrhello
+  args <- getArgs
+  let n = length args
+  alloca $ \p_argc -> do
+    poke p_argc (fromIntegral n)
+    p_argv <- newArray =<<  mapM newCString args
+    app <- newQApplication p_argc p_argv 
+    cstr <- newCString "Hello world"
+    hello <- newQLabel cstr
     qLabelshow hello 
     qApplicationexec app 
 
