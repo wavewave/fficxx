4433865ae638f2ba8662b740315ce282b049f136
Ian-Woo Kim <ianwookim@gmail.com>
2015-06-10 05:29:23 +0000

    use operator for data constructor

--- experiment/test2.hs
+++ experiment/test2.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
@@ -26,11 +27,34 @@ data N where
 data BHList :: N -> N -> N -> * -> * where
   E :: BHList Z Z Z a
   M :: BHList l m n a -> BHList l (S m) (S n) a
-  A :: a -> BHList l m n a -> BHList (S l) m (S n) a
+  (:+) :: a -> BHList l m n a -> BHList (S l) m (S n) a
 
+infixr 9 :+
+
+{-
+class Emptyable m where
+  empty :: m 
+
+
+instance Emptyable (BHList Z Z Z a) where
+  empty = E
+
+
+instance Emptyable (BHList Z (S Z) (S Z) a) where
+  empty = M E
+-}
+
+{-
+instance (Emptyable (BHList Z n n a)) => 
+           Emptyable (BHList Z (S n) (S n) a) where
+  empty = M empty
+-}
+
+-- g :: BHList Z n n a 
+-- g = 
 
 x :: BHList (T 3) (T 2) (T 5) Int
-x = 1 `A` (2 `A` (3 `A` (M (M E))))
+x = 1 :+ 2 :+ 3 :+ (M (M E))
 
 
 
@@ -39,10 +63,13 @@ y = M E
 
 
 f :: forall m. forall n. BHList (S (S (S m))) n  (T 5) Int -> Int
-f (x `A` (y `A` (z `A` (M (M E))))) = x + y + z
-f (x `A` (y `A` (z `A` (o1 `A` (M E))))) = x + y + z + o1
-f (x `A` (y `A` (z `A` (o1 `A` (o2 `A` E))))) = x + y + z + o1 + o2
+f (x :+ y :+ z :+ M (M E)) = x + y + z
+f (x :+ y :+ z :+ o1 :+ (M E)) = x + y + z + o1
+f (x :+ y :+ z :+ o1 :+ o2 :+ E) = x + y + z + o1 + o2
 
 
 main :: IO ()
-main = putStrLn "test"
+main = do
+  print (f (1 :+ 2 :+ 3 :+ M (M E)))
+  print (f (1 :+ 2 :+ 3 :+ 4 :+ M E))
+  print (f (1 :+ 2 :+ 3 :+ 4 :+ 5 :+ E))
