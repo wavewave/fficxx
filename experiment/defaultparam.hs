8df6198a669ef4dd04a5e691435ad3752b676817
Ian-Woo Kim <ianwookim@gmail.com>
2015-06-12 16:56:17 +0000

    experiment successful approach to default parameters using GADTs, type-level number and type-level list

--- experiment/defaultparam.hs
+++ experiment/defaultparam.hs
@@ -10,6 +10,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
+import Data.Proxy
 import GHC.TypeLits 
 
 type family EQ (a :: Nat) :: N
@@ -34,28 +35,30 @@ data N where
   Z :: N
   S :: N -> N
 
-data BHList :: N -> N -> N -> * -> * where
-  E :: BHList Z Z Z a
-  M :: BHList l m n a -> BHList l (S m) (S n) a
-  (:+) :: a -> BHList l m n a -> BHList (S l) m (S n) a
+data BHList :: N -> N -> N -> [*] -> * where
+  E :: BHList Z Z Z '[]
+  M :: Proxy x -> BHList l m n xs -> BHList l (S m) (S n) (x ': xs)
+  (:+) :: x -> BHList l m n xs -> BHList (S l) m (S n) (x ': xs)
 
 infixr 9 :+
 
-x :: BHList (EQ 3) (EQ 2) (EQ 5) Int
+{-
+x :: BHList (EQ 3) (EQ 2) (EQ 5) '[Int,Int,Int]
 x = 1 :+ 2 :+ 3 :+ (M (M E))
 
-y :: BHList (EQ 0) (EQ 1) (EQ 1) Int
+y :: BHList (EQ 0) (EQ 1) (EQ 1) '[Int]
 y = M E 
+-}
 
-
-f :: forall m. forall n. BHList (GTEQ m 3) n (EQ 5) Int -> Int
-f (x :+ y :+ z :+ M (M E)) = x + y + z
-f (x :+ y :+ z :+ o1 :+ (M E)) = x + y + z + o1
-f (x :+ y :+ z :+ o1 :+ o2 :+ E) = x + y + z + o1 + o2
+f :: forall m. forall n. BHList (GTEQ m 3) n (EQ 5) '[Int,String,Double,Int,Int] 
+  -> (Int,String,Double,Int,Int)
+f (x :+ y :+ z :+ M Proxy (M Proxy E)) = (x, y, z, 0 , 0 )
+f (x :+ y :+ z :+ o1 :+ (M Proxy E))   = (x, y, z, o1, 0 )
+f (x :+ y :+ z :+ o1 :+ o2 :+ E)       = (x, y, z, o1, o2)
 
 
 main :: IO ()
 main = do
-  print (f (1 :+ 2 :+ 3 :+ M (M E)))
-  print (f (1 :+ 2 :+ 3 :+ 4 :+ M E))
-  print (f (1 :+ 2 :+ 3 :+ 4 :+ 5 :+ E))
+  print (f (1 :+ "2" :+ 3.0 :+ M Proxy (M Proxy E)))
+  print (f (1 :+ "2" :+ 3.0 :+ 4 :+ M Proxy E))
+  print (f (1 :+ "2" :+ 3.0 :+ 4 :+ 5 :+ E))
