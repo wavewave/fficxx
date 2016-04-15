44b170834e79c1c451466425590ad98ee4b248f0
Ian-Woo Kim <ianwookim@gmail.com>
2016-04-15 00:28:39 +0000

    add haskell deps and remove unnecessary dep constraints on errors.

--- shell.nix
+++ shell.nix
@@ -4,7 +4,9 @@
 with pkgs;
 
 let hsenv = haskellPackages.ghcWithPackages
-      (p: with p; [ ] );
+      (p: with p; [ base-orphans cereal data-default dlist old-locale parallel parsec either errors lens 
+                    HStringTemplate crypto-api pureMD5 syb split
+       ] );
 in stdenv.mkDerivation {
      name = "fficxx-shell";
      buildInputs = [ hsenv snappy ];
