756ab4f77c4f408da80fa93a32da87ffe426dfc9
Ian-Woo Kim <ianwookim@gmail.com>
2016-04-15 00:24:01 +0000

    modern shell.nix

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+
+with pkgs;
+
+let hsenv = haskellPackages.ghcWithPackages
+      (p: with p; [ ] );
+in stdenv.mkDerivation {
+     name = "fficxx-shell";
+     buildInputs = [ hsenv snappy ];
+     shellHook= ''
+       eval $(grep export ${hsenv}/bin/ghc)
+     '';
+   }
+   
+#let newghcpkgs = pkgs.callPackage <nixpkgs/pkgs/development/haskell-modules> {
+#      ghc = pkgs.haskell.compiler.ghc7103;
+#      packageSetConfig = pkgs.callPackage <nixpkgs/pkgs/development/haskell-modules/configuration-ghc-7.10.x.nix> {};
+#      overrides = import ./override.nix { inherit pkgs; };
+#    };
+#    hsenv = newghcpkgs.ghcWithPackages (pkgs: with pkgs; [fficxx fficxx-runtime cabal-install]); 
+#in pkgs.stdenv.mkDerivation {
+#     name = "fficxx-use-env";
+#     buildInputs = [ hsenv pkgs.snappy ];
+#     shellHook = ''
+#       eval $(grep export ${hsenv}/bin/ghc)
+#     '';
+#   }
