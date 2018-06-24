d0e7d124db4a1d2dbab6f72e44e99c34361b7b6f
Ian-Woo Kim <ianwookim@gmail.com>
2018-06-24 19:09:57 +0000

    use callCabal2nix

--- fficxx/shell.nix
+++ fficxx/shell.nix
@@ -1,25 +1,11 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellPackages.callPackage
-            ({ mkDerivation, base, bytestring, Cabal, containers
-             , data-default, directory
-             , either, errors, filepath, hashable, lens, mtl
-             , process, pureMD5, split, stdenv, template-haskell, transformers
-             , unordered-containers
-             , template, haskell-src-exts
-             }:
-             mkDerivation {
-               pname = "fficxx";
-               version = "0.2.0";
-               src = ./.;
-               buildDepends = [
-                 base bytestring Cabal containers data-default directory either errors filepath
-                 hashable lens mtl process pureMD5 split
-                 template-haskell transformers unordered-containers
-                 template haskell-src-exts
-               ];
-	       buildTools = [ haskellPackages.cabal-install pkgs.snappy ];
-               description = "automatic C++ binding generation";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let
+
+  fficxx = haskellPackages.callCabal2nix "fficxx" ./. {};
+
 in
-  pkg.env
+
+  fficxx.env
