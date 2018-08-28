e0dadec9db45753ec4e4cc06f1ddb8f1cf2c036d
Ian-Woo Kim <ianwookim@gmail.com>
2018-08-28 23:54:33 +0000

    haskell closure test.

--- test/function/experiment/hsMain.hs
+++ test/function/experiment/hsMain.hs
@@ -13,11 +13,25 @@ test = do
   putStrLn "haskell test"
 
 
+closureTest :: Int -> IO ()
+closureTest x = do
+  putStrLn "closure test"
+  putStrLn (" x = " ++ show x)
+
 main = do
   putStrLn "hsMain"
-  p_test <- createIO test
-  Function fptr <- c_Function_new_f1 p_test
 
-  c_Function_call_f1 fptr
+  putStrLn "------------"
+  p_test1 <- createIO test
+  Function fptr1 <- c_Function_new_f1 p_test1
+  c_Function_call_f1 fptr1
+
+  putStrLn "------------"
+  p_test2 <- createIO (closureTest 32)
+  Function fptr2 <- c_Function_new_f1 p_test2
+  c_Function_call_f1 fptr2
+
+
+
 
   pure ()
