80a8fded0bb217c62c36fa7bdf08938b8018c340
Ian-Woo Kim <ianwookim@gmail.com>
2018-08-29 04:21:40 +0000

    reorg header/cc files. remove main.c

--- test/function/experiment/function_test.h
+++ test/function/experiment/function_test.h
@@ -2,26 +2,6 @@
 extern "C" {
 #endif
 
-// testing void
-void* newFunction1( void(*fp)() );
-
-void callFunction1( void* stdfn );
-
-// testing param
-void* newFunction2( void(*fp)(int) );
-
-void callFunction2( void* stdfn, int x );
-
-// testing return
-void* newFunction3( int(*fp)(int) );
-
-int callFunction3( void* stdfn, int x );
-
-// testing multiparam
-void* newFunction4( int(*fp)(int,char) );
-
-int callFunction4( void* stdfn, int x, char y);
-
 #define VARGS(...) __VA_ARGS__
 
 #define TAIL(x,...) __VA_ARGS__
@@ -29,32 +9,50 @@ int callFunction4( void* stdfn, int x, char y);
 #define UNPAREN(x) VARGS x
 
 
-#define Function_new(NAME,R,ATYPS)\
+#define Function_new_decl(NAME,R,ATYPS)\
   void* Function_new_ ## NAME ( R (*fp) ( UNPAREN(ATYPS) ) );
 
-#define Function_call(NAME,R,ATYPS)\
+#define Function_call_decl(NAME,R,ATYPS)\
   R Function_call_ ## NAME ( UNPAREN(ATYPS) );
 
 
-Function_new(f1,void,())
+#define Function_new_inst(NAME,R,ATYPS)                     \
+    inline void* Function_new_ ## NAME ( R (*fp)( UNPAREN(ATYPS) ) ) \
+  {\
+      std::function< R ( UNPAREN(ATYPS) )>* p = new std::function< R ( UNPAREN(ATYPS) ) >(fp); \
+    return static_cast<void*>(p);\
+  }\
+  auto a_Function_new_ ## NAME = Function_new_ ## NAME;
 
-Function_call(f1,void,(void* op))
 
-Function_new(f2,void,(int x))
+#define Function_call_inst(NAME,R,ATYPS,AVARS)        \
+  inline R Function_call_ ## NAME ( UNPAREN(ATYPS) ) \
+  {\
+      std::function< R (TAIL ATYPS) >* p = static_cast< std::function< R (TAIL ATYPS) >* >(op); \
+    return (*p) AVARS;                                 \
+  }\
+  auto a_Function_call_ ## NAME = Function_call_ ## NAME;
 
-Function_call(f2,void,(void* op,int x))
 
-Function_new(f3,int,(int x))
+Function_new_decl(f1,void,())
 
-Function_call(f3,int,(void* op,int x))
+Function_call_decl(f1,void,(void* op))
 
-Function_new(f4,int,(int x,char y))
+Function_new_decl(f2,void,(int x))
 
-Function_call(f4,int,(void* op,int x,char y))
+Function_call_decl(f2,void,(void* op,int x))
 
-Function_new(f5,int,())
+Function_new_decl(f3,int,(int x))
 
-Function_call(f5,int,(void* op))
+Function_call_decl(f3,int,(void* op,int x))
+
+Function_new_decl(f4,int,(int x,char y))
+
+Function_call_decl(f4,int,(void* op,int x,char y))
+
+Function_new_decl(f5,int,())
+
+Function_call_decl(f5,int,(void* op))
 
 
 #ifdef __cplusplus
