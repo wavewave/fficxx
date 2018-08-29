c5adaa8e2546527505396acf16959f82ec482b29
Ian-Woo Kim <ianwookim@gmail.com>
2018-08-29 04:22:29 +0000

    rename .cc -> .cpp

--- test/function/experiment/build.sh
+++ test/function/experiment/build.sh
@@ -4,7 +4,7 @@ BASEDIR=${GHCDIR}/../lib/ghc-8.0.2
 rm function_test.o
 rm hsMain.o
 
-g++ -c function_test.cc -I${BASEDIR}/fficxx-runtime-0.3/include
+g++ -c function_test.cpp -I${BASEDIR}/fficxx-runtime-0.3/include
 ghc -c Binding.hs
 ghc -c hsMain.hs
 ghc -o hsMain hsMain.hs Binding.hs function_test.o -lstdc++
