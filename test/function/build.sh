ceaa28bde92b66bf2dfebb92f53fdffb74ee3a69
Ian-Woo Kim <ianwookim@gmail.com>
2018-08-29 04:15:33 +0000

    remove unnecessary files (remnant of rebase)

--- test/function/build.sh
+++
@@ -1,8 +0,0 @@
-GHCDIR=$(dirname $(which ghc))
-BASEDIR=${GHCDIR}/../lib/ghc-8.0.2
-
-g++ -c stub.cc -I${BASEDIR}/stdcxx-0.0/include -I${BASEDIR}/fficxx-runtime-0.3/include
-ghc -c -ddump-splices test.hs
-ghc test.hs stub.o
-
-
