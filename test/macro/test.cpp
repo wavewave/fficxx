74ade1d2f434499c20b05b78e9e9e942aae470ce
Ian-Woo Kim <ianwookim@gmail.com>
2018-08-31 00:06:52 +0000

    did my best up to this point.

--- test/macro/test.cpp
+++ test/macro/test.cpp
@@ -14,60 +14,69 @@ MAP(GETVAR, COMMA, (int,x), (char*,y), (A,z) )
 
 #define UNPAREN(x) VARGS x
 
+#define GETTYPES(x) MAP(GETTYPE,COMMA,UNPAREN(x))
+#define GETVARS(x) MAP(GETVAR,COMMA,UNPAREN(x))
 
-#define Function_new_decl(NAME,R,ATYPS)\
-    void* Function_new_ ## NAME ( R (*fp) ( UNPAREN(ATYPS) ));
+
+#define Function_new_decl(NAME,R,ATYPS)                         \
+    void* Function_new_ ## NAME ( R (*fp) ( GETTYPES(ATYPS) ));
 
 #define Function_call_decl(NAME,R,ATYPS)                                \
-    R Function_call_ ## NAME ( REMOVE_TRAILING_COMMAS( void*, UNPAREN(ATYPS) ) );
+    R Function_call_ ## NAME ( REMOVE_TRAILING_COMMAS( void*, GETTYPES(ATYPS) ) );
 
 #define Function_delete_decl(NAME,R,ATYPS)           \
     void Function_delete_ ## NAME ( void*  );
 
-#define Function_new_inst(NAME,R,ATYPS,AVARS)                                \
-    inline void* Function_new_ ## NAME ( R (*fp)( UNPAREN(ATYPS) ) )    \
+#define Function_new_inst(NAME,R,ATYPS)                                \
+    inline void* Function_new_ ## NAME ( R (*fp)( GETTYPES(ATYPS) ) )   \
     {                                                                   \
-        std::function< R ( UNPAREN(ATYPS) )>* p = new std::function< R ( UNPAREN(ATYPS) ) >(fp); \
+        std::function< R ( GETTYPES(ATYPS) )>* p = new std::function< R ( GETTYPES(ATYPS) ) >(fp); \
         return static_cast<void*>(p);                                   \
     }                                                                   \
     auto a_Function_new_ ## NAME = Function_new_ ## NAME;
 
 
-#define Function_call_inst(NAME,R,ATYPS,AVARS)        \
+#define Function_call_inst(NAME,R,ATYPS)        \
     inline R Function_call_ ## NAME ( REMOVE_TRAILING_COMMAS( void* op,  UNPAREN(ATYPS) ) ) \
     {                                                                   \
-        std::function< R (UNPAREN(ATYPS)) >* p = static_cast< std::function< R (UNPAREN(ATYPS)) >* >(op); \
-        return (*p) AVARS;                                              \
+        std::function< R ( GETTYPES(ATYPS) )>* p = static_cast< std::function< R ( GETTYPES(ATYPS) )>* >(op); \
+        return (*p) ( GETVARS(ATYPS) );                              \
     }                                                                   \
     auto a_Function_call_ ## NAME = Function_call_ ## NAME;
 
-#define Function_delete_inst(NAME,R,ATYPS,AVARS)             \
+#define Function_delete_inst(NAME,R,ATYPS)             \
     inline void Function_delete_ ## NAME ( void* op )   \
     {                                                                   \
-        std::function< R (UNPAREN(ATYPS)) >* p = static_cast< std::function< R (UNPAREN(ATYPS)) >* >(op); \
+        std::function< R ( GETTYPES(ATYPS) ) >* p = static_cast< std::function< R ( GETTYPES(ATYPS) ) >* >(op); \
         delete p;                                                       \
     }                                                                   \
     auto a_Function_delete_ ## NAME = Function_delete_ ## NAME;
 
 
-Function_new_decl(f1,void,())
+Function_new_decl(f1,void,(()))
 
-Function_call_decl(f1,void,())
+Function_call_decl(f1,void,(()))
 
-Function_delete_decl(f1,void,())
+Function_delete_decl(f1,void,(()))
+
+Function_new_inst(f1,void,(()))
+
+Function_call_inst(f1,void,(()))
+
+Function_delete_inst(f1,void,(()))
 
 
-Function_new_decl(f4,int,(int,char))
+Function_new_decl(f4,int,((int,x),(char,y)))
 
-Function_call_decl(f4,int,(int,char))
+Function_call_decl(f4,int,((int,x),(char,y)))
 
-Function_delete_decl(f4,int,(int,char))
+Function_delete_decl(f4,int,((int,x),(char,y)))
 
-Function_new_inst(f4,int,(int,char),(x,y))
+Function_new_inst(f4,int,((int,x),(char,y)))
 
-Function_call_inst(f4,int,(int,char),(x,y))
+Function_call_inst(f4,int,((int,x),(char,y)))
 
-Function_delete_inst(f4,int,(int,char),(x,y))
+Function_delete_inst(f4,int,((int,x),(char,y)))
 
 
 /*
@@ -86,3 +95,23 @@ Function_delete_inst(f5,int,(void* op))
 
 VARGS()
 // MAP( GETTYPE,COMMA )
+
+
+#define TEST(X,...)   __VA_ARGS__
+
+FIRST()
+
+TEST(1,)
+TEST(2,)
+TEST(3,x,y)
+
+MAP(GETTYPE,COMMA,())
+MAP(GETTYPE,COMMA,(int,x))
+MAP(GETTYPE,COMMA,(x),(y))
+FIRST()
+
+
+
+
+SECOND(x,y)
+MAP(GETVAR,COMMA,())
