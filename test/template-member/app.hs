29dc6049d651c00ceba1ee9604e28a761ba19468
Ian-Woo Kim <ianwookim@gmail.com>
2018-09-02 04:58:40 +0000

    first working template member function generating TH code. this TH code should be generated.

--- test/template-member/app.hs
+++ test/template-member/app.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import TestPkg.A
+import TestPkg.A.Implementation
 import TestPkg.T1
+
+import TH
+
+$(instance_a_method [t|T1|] "T1")
 
 main :: IO ()
 main = do
   a <- newA
   t1 <- newT1
   t1_print t1
-  a_methodT1 a t1
+  a_method_T1 a t1
   pure ()
--- test/template-member/build.sh
+++ test/template-member/build.sh
@@ -1,13 +1,18 @@
-g++ -c test.cpp
-g++ -c main.cpp
-g++ -o main main.o test.o
+GHCDIR=$(dirname $(which ghc))
+BASEDIR=${GHCDIR}/../lib/ghc-8.0.2
 
-rm Gen.o
-rm -rf working
-ghc Gen.hs && ./Gen && cd testpkg && cabal clean && cd ..
+#g++ -c test.cpp
+#g++ -c main.cpp
+#g++ -o main main.o test.o
+
+#rm Gen.o
+#rm -rf working
+#ghc Gen.hs && ./Gen
+cd testpkg && cabal clean && cd ..
 
 cabal sandbox delete && cabal sandbox init && cabal sandbox add-source testpkg && cabal install testpkg
 
+g++ -c stub.cc -I${BASEDIR}/stdcxx-0.5/include -I${BASEDIR}/fficxx-runtime-0.5/include -Itestpkg/csrc
 cabal exec -- ghc -c app.hs
-cabal exec -- ghc app.hs
+cabal exec -- ghc app.hs stub.o
 
