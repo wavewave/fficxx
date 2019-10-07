8168f6448d9364da6863346ca8dbd5cd56280de6
Ian-Woo Kim <ianwookim@gmail.com>
2019-10-07 12:22:47 -0700

    remove default.nix..

--- fficxx-runtime/default.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, bytestring, stdenv, template-haskell }:
-mkDerivation {
-  pname = "fficxx-runtime";
-  version = "0.3";
-  src = ./.;
-  libraryHaskellDepends = [ base bytestring template-haskell ];
-  description = "Runtime for fficxx-generated library";
-  license = stdenv.lib.licenses.bsd3;
-}
