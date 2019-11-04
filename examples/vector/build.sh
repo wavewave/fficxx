96f675cfbfd262ef2b17277d59858b30c319c45e
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-03 21:54:42 -0800

    unique_ptr build

--- examples/vector/build.sh
+++
@@ -1,3 +0,0 @@
-g++ -c stub.cc -I../../stdcxx/csrc -I../../fficxx-runtime/csrc
-ghc -c test.hs
-ghc test.hs stub.o
--- workspace/build.sh
+++ workspace/build.sh
@@ -2,6 +2,13 @@ rm -rf dist-newstyle
 cd ../stdcxx-gen; ghc Gen.hs ; cd ../workspace
 ../stdcxx-gen/Gen
 cabal new-build stdcxx
-cabal new-exec -- g++ -c vector/stub.cc -I stdcxx/csrc  -I../fficxx-runtime/csrc/
+
+# vector
+cabal new-exec -- g++ -c vector/stub.cc -o vector/stub.o -I stdcxx/csrc  -I../fficxx-runtime/csrc/
 cabal new-exec -- ghc -c vector/test.hs
-cabal new-exec -- ghc vector/test.hs stub.o
+cabal new-exec -- ghc vector/test.hs vector/stub.o
+
+# unique_ptr
+cabal new-exec -- g++ -c unique_ptr/stub.cc -o unique_ptr/stub.o -I stdcxx/csrc  -I../fficxx-runtime/csrc/
+cabal new-exec -- ghc -c unique_ptr/test.hs
+cabal new-exec -- ghc unique_ptr/test.hs unique_ptr/stub.o
