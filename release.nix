97112a3233a2ace66034519cc0ee36f82ea03336
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-03 20:49:40 -0800

    add release.nix

---
+++ release.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let
+
+  newHaskellPackages0 = haskellPackages.override {
+    overrides = self: super: {
+      "fficxx-runtime" = self.callCabal2nix "fficxx-runtime" ./fficxx-runtime {};
+      "fficxx"         = self.callCabal2nix "fficxx"         ./fficxx         {};
+    };
+  };
+
+  stdcxxNix = import ./stdcxx-gen/default.nix {
+    inherit stdenv;
+    haskellPackages = newHaskellPackages0;
+  };
+
+  newHaskellPackages = haskellPackages.override {
+    overrides = self: super:
+      {
+        "fficxx-runtime" = self.callCabal2nix "fficxx-runtime" ./fficxx-runtime {};
+        "fficxx"         = self.callCabal2nix "fficxx"         ./fficxx         {};
+        "stdcxx"         = self.callPackage stdcxxNix {};
+      };
+  };
+
+in
+
+{
+
+  "fficxx"         = newHaskellPackages.fficxx;
+  "fficxx-runtime" = newHaskellPackages.fficxx-runtime;
+  "stdcxx"         = newHaskellPackages.stdcxx;
+
+}
