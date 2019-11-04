16c40d3fc5654e3f8c2289a46629fd62b151e3b1
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-03 21:48:21 -0800

    stdcxx example build fix

--- workspace/build.sh
+++ workspace/build.sh
@@ -2,6 +2,6 @@ rm -rf dist-newstyle
 cd ../stdcxx-gen; ghc Gen.hs ; cd ../workspace
 ../stdcxx-gen/Gen
 cabal new-build stdcxx
-cabal new-exec -- g++ -c vector/stub.cc # -I../stdcxx/csrc -I../fficxx-runtime/csrc/
+cabal new-exec -- g++ -c vector/stub.cc -I stdcxx/csrc  -I../fficxx-runtime/csrc/
 cabal new-exec -- ghc -c vector/test.hs
-cabal new-exec -- ghc test.hs stub.o
+cabal new-exec -- ghc vector/test.hs stub.o
