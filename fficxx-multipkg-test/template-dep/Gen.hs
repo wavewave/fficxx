6ad617478fb2642dbc3a65ab04511ab48753dd25
Ian-Woo Kim <ianwookim@gmail.com>
2020-01-19 02:44:38 -0800

    add type sig in tmeplate-dep/Gen.hs

--- fficxx-multipkg-test/template-dep/Gen.hs
+++ fficxx-multipkg-test/template-dep/Gen.hs
@@ -56,77 +56,6 @@ import FFICXX.Generate.Type.Class
 import FFICXX.Generate.Type.Module
 import FFICXX.Generate.Type.PackageInterface
 
-{-
--- -------------------------------------------------------------------
--- import from stdcxx
--- -------------------------------------------------------------------
-
-stdcxx_cabal :: Cabal
-stdcxx_cabal = Cabal {
-    cabal_pkgname            = CabalName "stdcxx"
-  , cabal_version            = "0.6"
-  , cabal_cheaderprefix      = "STD"
-  , cabal_moduleprefix       = "STD"
-  , cabal_additional_c_incs  = []
-  , cabal_additional_c_srcs  = []
-  , cabal_additional_pkgdeps = []
-  , cabal_license            = Nothing
-  , cabal_licensefile        = Nothing
-  , cabal_extraincludedirs   = []
-  , cabal_extralibdirs       = []
-  , cabal_extrafiles         = []
-  , cabal_pkg_config_depends = []
-  , cabal_buildType          = Simple
-  }
-
--- import from stdcxx
-deletable :: Class
-deletable =
-  AbstractClass {
-    class_cabal      = stdcxx_cabal
-  , class_name       = "Deletable"
-  , class_parents    = []
-  , class_protected  = Protected []
-  , class_alias      = Nothing
-  , class_funcs      = [ Destructor Nothing ]
-  , class_vars       = []
-  , class_tmpl_funcs = []
-  }
-
--- import from stdcxx
-string :: Class
-string =
-  Class stdcxx_cabal "string" [ deletable ] mempty
-    (Just (ClassAlias { caHaskellName = "CppString", caFFIName = "string"}))
-    [ Constructor [ cstring "p" ] Nothing
-    , NonVirtual cstring_ "c_str" [] Nothing
-    , NonVirtual (cppclassref_ string) "append" [cppclassref string "str"] Nothing
-    , NonVirtual (cppclassref_ string) "erase" [] Nothing
-    ]
-    []
-    []
-    False
-
-t_vector :: TemplateClass
-t_vector = TmplCls stdcxx_cabal "Vector" "std::vector" ["tp1"]
-             [ TFunNew [] Nothing
-             , TFun void_ "push_back" "push_back"   [Arg (TemplateParam "tp1") "x"] Nothing
-             , TFun void_ "pop_back"  "pop_back"    []                        Nothing
-             , TFun (TemplateParam "tp1") "at" "at" [int "n"]                 Nothing
-             , TFun int_  "size"      "size"        []                        Nothing
-             , TFunDelete
-             ]
-
-t_unique_ptr :: TemplateClass
-t_unique_ptr = TmplCls stdcxx_cabal "UniquePtr" "std::unique_ptr" ["tp1"]
-             [ TFunNew [] (Just "newUniquePtr0")
-             , TFunNew [Arg (TemplateParamPointer "tp1") "p"] Nothing
-             , TFun (TemplateParamPointer "tp1") "get" "get" [] Nothing
-             , TFun (TemplateParamPointer "tp1") "release" "release" [] Nothing
-             , TFun void_ "reset" "reset" [] Nothing
-             , TFunDelete
-             ]
--}
 
 -- -------------------------------------------------------------------
 -- tmpl-dep-test
@@ -150,8 +79,10 @@ cabal_ tdtestH tdtestCpp =
         , cabal_buildType          = Simple
         }
 
+extraDep :: [(String,[String])]
 extraDep = []
 
+extraLib :: [String]
 extraLib = []
 
 tT1 :: Cabal -> TemplateClass
@@ -207,19 +138,21 @@ tT2 cabal =
     ]
   }
 
-
+classes :: Cabal -> [Class]
 classes cabal = [ ]
 
+toplevelfunctions :: [TopLevelFunction]
 toplevelfunctions = [ ]
 
+templates :: Cabal -> [TemplateClassImportHeader]
 templates cabal =
-  [ TCIH (tT1 cabal) [ "tdtest.h" ]
-  , TCIH (tT2 cabal) [ "tdtest.h" ]
+  [ TCIH (tT1 cabal) ["tdtest.h"]
+  , TCIH (tT2 cabal) ["tdtest.h"]
   ]
 
+headers :: [(ModuleUnit, ModuleUnitImports)]
 headers =
   [ ]
-
 
 main :: IO ()
 main = do
