1dc1e2777d63387ac74ab2d2864d56f30a4a5297
Ian-Woo Kim <ianwookim@gmail.com>
2020-05-22 11:32:34 -0700

    created problematic case with duplicated template instatiation in different modules.

--- fficxx-multipkg-test/tmpl-dup-inst/src/Test/TemplateInstantiation/A.hs
+++ fficxx-multipkg-test/tmpl-dup-inst/src/Test/TemplateInstantiation/A.hs
@@ -1,1 +1,41 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE TemplateHaskell          #-}
+
 module Test.TemplateInstantiation.A where
+
+import           Foreign.C.Types             (CInt(..))
+--
+import           FFICXX.Runtime.TH           (IsCPrimitive (..),
+                                              TemplateParamInfo (..))
+import           STD.Vector.Template
+import qualified STD.Vector.TH               as TH
+import           STD.VectorIterator.Template
+import qualified STD.VectorIterator.TH       as TH
+
+
+TH.genVectorIteratorInstanceFor
+  CPrim
+  ( [t|CInt|], TPInfo { tpinfoCxxType       = "int"
+                      , tpinfoCxxHeaders    = []
+                      , tpinfoCxxNamespaces = []
+                      , tpinfoSuffix        = "int"
+                      }
+  )
+
+TH.genVectorInstanceFor
+  CPrim
+  ( [t|CInt|], TPInfo { tpinfoCxxType       = "int"
+                      , tpinfoCxxHeaders    = []
+                      , tpinfoCxxNamespaces = []
+                      , tpinfoSuffix        = "int"
+                      }
+  )
+
+test :: IO ()
+test = do
+  v <- newVector :: IO (Vector CInt)
+  n <- size v
+  print n
+  push_back v 1
+  n' <- size v
+  print n'
