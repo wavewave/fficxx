d2ef7a843e70c8565df5d25041ed80fe9163be30
Ian-Woo Kim <ianwookim@gmail.com>
2020-06-12 22:20:58 -0700

    ormolu formatting.

--- fficxx-multipkg-test/test/TemplateTopLevelSpec.hs
+++ fficxx-multipkg-test/test/TemplateTopLevelSpec.hs
@@ -3,39 +3,46 @@
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -w #-}
 
-module TemplateTopLevelSpec ( spec ) where
+module TemplateTopLevelSpec
+  ( spec,
+  )
+where
 
-import Foreign.C.Types    ( CInt(..) )
-import System.IO.Silently ( capture_ )
 --
-import FFICXX.Runtime.TH  ( IsCPrimitive(..), TemplateParamInfo(..) )
+import FFICXX.Runtime.TH (IsCPrimitive (..), TemplateParamInfo (..))
+import Foreign.C.Types (CInt (..))
 import STD.Vector.Template
+import System.IO.Silently (capture_)
 --
+
+--
+import Test.Hspec (Spec, afterAll, around, beforeAll, describe, it, shouldBe)
 import TmplTopLevelTest
 import TmplTopLevelTest.TH (genReturn_vectorInstanceFor)
---
-import Test.Hspec     ( Spec, afterAll, around, beforeAll, describe, it, shouldBe )
 
 genReturn_vectorInstanceFor
   CPrim
-  ( [t|CInt|], TPInfo { tpinfoCxxType       = "int"
-                      , tpinfoCxxHeaders    = []
-                      , tpinfoCxxNamespaces = []
-                      , tpinfoSuffix        = "int"
-                      }
+  ( [t|CInt|],
+    TPInfo
+      { tpinfoCxxType = "int",
+        tpinfoCxxHeaders = [],
+        tpinfoCxxNamespaces = [],
+        tpinfoSuffix = "int"
+      }
   )
 
 spec :: Spec
 spec =
-  describe "call top-level function" $ do
-    it "should call ordinary top-level function" $ do
-      let action = do
-            ordinary :: IO ()
-      s <- capture_ action
-      s `shouldBe` "ordinary function is called\n"
-    it "should call template top-level function" $ do
-      let action = do
-            return_vector @CInt (12 :: CInt) -- :: IO ()
-      s <- capture_ action
-      s `shouldBe` "template function is called\n"
-
+  describe
+    "call top-level function"
+    $ do
+      it "should call ordinary top-level function" $ do
+        let action = do
+              ordinary :: IO ()
+        s <- capture_ action
+        s `shouldBe` "ordinary function is called\n"
+      it "should call template top-level function" $ do
+        let action = do
+              return_vector @CInt (12 :: CInt) -- :: IO ()
+        s <- capture_ action
+        s `shouldBe` "template function is called\n"
