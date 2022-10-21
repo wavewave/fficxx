36cde73a7be35848ae867715551c2c8b66211ff7
Ian-Woo Kim <ianwookim@gmail.com>
2022-10-21 11:26:56 -0700

    Simplify Nix script and support for multiple GHC versions in build (#196)
    In the end, we do not need multi-stage haskellPackages for generating source code from fficxx.
    Now, the nix build script supports multiple version of GHC easily.
    
    * remove newHaskellPackages0
    * devshell fix
    * ditch providing nixpkgs-level overlay that overries default haskellPackages.
    instead, just provide haskellOverlay
    * no more use default haskellPackages
    * support multiple versions of GHC
    * support GHC 9.4.2!!
    * CI update. use GHC 9.4.2 for now.

--- flake.nix
+++ flake.nix
@@ -1,97 +1,79 @@
 {
   description = "fficxx";
   inputs = {
-    # nixpkgs/master on 2022-07-18
-    nixpkgs.url = "github:NixOS/nixpkgs/31997025a4d59f09a9b4c55a3c6ff5ade48de2d6";
+    # nixpkgs/master on 2022-10-21
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/71c5816834f93840dd301ec384c9d7947e97c27d";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        haskellPackages = pkgs.haskellPackages;
-
-        newHaskellPackages0 = haskellPackages.override {
-          overrides = self: super: {
-            "fficxx-runtime" =
-              self.callCabal2nix "fficxx-runtime" ./fficxx-runtime { };
-            "fficxx" = self.callCabal2nix "fficxx" ./fficxx { };
+        haskellOverlay = final: hself: hsuper: rec {
+          stdcxxSrc = import ./stdcxx-gen/gen.nix {
+            inherit (final) lib stdenv;
+            haskellPackages = hself;
           };
-        };
+          tmfTestSrc = import ./fficxx-multipkg-test/template-member/gen.nix {
+            inherit (final) stdenv;
+            haskellPackages = hself;
+          };
+          tmplDepTestSrc = import ./fficxx-multipkg-test/template-dep/gen.nix {
+            inherit (final) stdenv;
+            haskellPackages = hself;
+          };
+          tmplTopLevelTestSrc =
+            import ./fficxx-multipkg-test/template-toplevel/gen.nix {
+              inherit (final) stdenv;
+              haskellPackages = hself;
+            };
 
-        stdcxxSrc = import ./stdcxx-gen/gen.nix {
-          inherit (pkgs) lib stdenv;
-          haskellPackages = newHaskellPackages0;
-        };
+          "template" = final.haskell.lib.doJailbreak (hsuper.template);
 
-        finalHaskellOverlay = self: super: {
           "fficxx-runtime" =
-            self.callCabal2nix "fficxx-runtime" ./fficxx-runtime { };
-          "fficxx" = self.callCabal2nix "fficxx" ./fficxx { };
-          "stdcxx" = self.callCabal2nix "stdcxx" stdcxxSrc { };
+            hself.callCabal2nix "fficxx-runtime" ./fficxx-runtime { };
+          "fficxx" = hself.callCabal2nix "fficxx" ./fficxx { };
+          "stdcxx" = hself.callCabal2nix "stdcxx" stdcxxSrc { };
+          "fficxx-test" = hself.callCabal2nix "fficxx-test" ./fficxx-test { };
+          "fficxx-multipkg-test" =
+            hself.callCabal2nix "fficxx-multipkg-test" ./fficxx-multipkg-test
+            { };
+          "tmf-test" = hself.callCabal2nix "tmf-test" tmfTestSrc { };
+          "tmpl-dep-test" =
+            hself.callCabal2nix "tmpl-dep-test" tmplDepTestSrc { };
+          "tmpl-dup-inst" = hself.callCabal2nix "tmpl-dup-inst"
+            ./fficxx-multipkg-test/tmpl-dup-inst { };
+          "tmpl-toplevel-test" =
+            hself.callCabal2nix "tmpl-toplevel-test" tmplTopLevelTestSrc { };
+
         };
 
-        newHaskellPackages = haskellPackages.override {
-          overrides = let
-            tmfTestSrc = import ./fficxx-multipkg-test/template-member/gen.nix {
-              inherit (pkgs) stdenv;
-              haskellPackages = newHaskellPackages0;
-            };
-            tmplDepTestSrc = import ./fficxx-multipkg-test/template-dep/gen.nix {
-              inherit (pkgs) stdenv;
-              haskellPackages = newHaskellPackages0;
-            };
-            tmplTopLevelTestSrc =
-              import ./fficxx-multipkg-test/template-toplevel/gen.nix {
-                inherit (pkgs) stdenv;
-                haskellPackages = newHaskellPackages0;
-              };
+        pkgs = import nixpkgs { inherit system; };
 
-          in self: super:
-          finalHaskellOverlay self super // {
-            "fficxx-test" = self.callCabal2nix "fficxx-test" ./fficxx-test { };
-            "fficxx-multipkg-test" =
-              self.callCabal2nix "fficxx-multipkg-test" ./fficxx-multipkg-test
-              { };
-            "tmf-test" = self.callCabal2nix "tmf-test" tmfTestSrc { };
-            "tmpl-dep-test" =
-              self.callCabal2nix "tmpl-dep-test" tmplDepTestSrc { };
-            "tmpl-dup-inst" = self.callCabal2nix "tmpl-dup-inst"
-              ./fficxx-multipkg-test/tmpl-dup-inst { };
-            "tmpl-toplevel-test" =
-              self.callCabal2nix "tmpl-toplevel-test" tmplTopLevelTestSrc { };
+        hpkgsFor = compiler:
+          pkgs.haskell.packages.${compiler}.extend (haskellOverlay pkgs);
+
+        mkPackages = compiler: {
+          inherit (hpkgsFor compiler)
+            fficxx fficxx-runtime stdcxx fficxx-test fficxx-multipkg-test
+            tmf-test tmpl-dep-test tmpl-dup-inst tmpl-toplevel-test;
+        };
+
+        mkShellFor = compiler:
+          (hpkgsFor compiler).shellFor {
+            packages = ps: [ ps.fficxx ps.fficxx-runtime ];
+            buildInputs = [ pkgs.cabal-install pkgs.ormolu pkgs.nixfmt ];
+            withHoogle = false;
           };
-        };
 
+        supportedCompilers = [ "ghc902" "ghc924" "ghc942" ];
       in {
-        packages = {
-          "stdcxx-src" = stdcxxSrc;
-          inherit (newHaskellPackages)
-            fficxx fficxx-runtime stdcxx fficxx-test fficxx-multipkg-test tmf-test
-            tmpl-dep-test tmpl-dup-inst tmpl-toplevel-test;
-        };
+        packages =
+          pkgs.lib.genAttrs supportedCompilers (compiler: hpkgsFor compiler);
 
-        # see these issues and discussions:
-        # - https://github.com/NixOS/nixpkgs/issues/16394
-        # - https://github.com/NixOS/nixpkgs/issues/25887
-        # - https://github.com/NixOS/nixpkgs/issues/26561
-        # - https://discourse.nixos.org/t/nix-haskell-development-2020/6170
-        overlay = final: prev: {
-          haskellPackages = prev.haskellPackages.override (old: {
-            overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
-              finalHaskellOverlay;
-          });
-        };
+        inherit haskellOverlay;
 
-        devShell = newHaskellPackages0.shellFor {
-          packages = ps: [ ps.fficxx ps.fficxx-runtime ];
-          buildInputs = [ pkgs.cabal-install pkgs.ormolu ];
-          withHoogle = false;
-        };
-      }
-  );
+        devShells =
+          pkgs.lib.genAttrs supportedCompilers (compiler: mkShellFor compiler);
+      });
 }
-
