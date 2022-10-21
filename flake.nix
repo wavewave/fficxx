00c6940a04d994f1187917f78ff0bf136d6cc3e9
Ian-Woo Kim <ianwookim@gmail.com>
2022-10-21 00:47:58 -0700

    remove newHaskellPackages0

--- flake.nix
+++ flake.nix
@@ -11,64 +11,49 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-
-        haskellPackages = pkgs.haskellPackages;
-
-        newHaskellPackages0 = haskellPackages.override {
-          overrides = self: super: {
-            "fficxx-runtime" =
-              self.callCabal2nix "fficxx-runtime" ./fficxx-runtime { };
-            "fficxx" = self.callCabal2nix "fficxx" ./fficxx { };
+        finalHaskellOverlay = self: super: rec {
+          stdcxxSrc = import ./stdcxx-gen/gen.nix {
+            inherit (pkgs) lib stdenv;
+            haskellPackages = self;
           };
-        };
+          tmfTestSrc = import ./fficxx-multipkg-test/template-member/gen.nix {
+            inherit (pkgs) stdenv;
+            haskellPackages = self;
+          };
+          tmplDepTestSrc = import ./fficxx-multipkg-test/template-dep/gen.nix {
+            inherit (pkgs) stdenv;
+            haskellPackages = self;
+          };
+          tmplTopLevelTestSrc =
+            import ./fficxx-multipkg-test/template-toplevel/gen.nix {
+              inherit (pkgs) stdenv;
+              haskellPackages = self;
+            };
 
-        stdcxxSrc = import ./stdcxx-gen/gen.nix {
-          inherit (pkgs) lib stdenv;
-          haskellPackages = newHaskellPackages0;
-        };
 
-        finalHaskellOverlay = self: super: {
           "fficxx-runtime" =
             self.callCabal2nix "fficxx-runtime" ./fficxx-runtime { };
           "fficxx" = self.callCabal2nix "fficxx" ./fficxx { };
           "stdcxx" = self.callCabal2nix "stdcxx" stdcxxSrc { };
+          "fficxx-multipkg-test" =
+            self.callCabal2nix "fficxx-multipkg-test" ./fficxx-multipkg-test
+            { };
+          "tmf-test" = self.callCabal2nix "tmf-test" tmfTestSrc { };
+          "tmpl-dep-test" =
+            self.callCabal2nix "tmpl-dep-test" tmplDepTestSrc { };
+          "tmpl-dup-inst" = self.callCabal2nix "tmpl-dup-inst"
+            ./fficxx-multipkg-test/tmpl-dup-inst { };
+          "tmpl-toplevel-test" =
+            self.callCabal2nix "tmpl-toplevel-test" tmplTopLevelTestSrc { };
+
         };
 
-        newHaskellPackages = haskellPackages.override {
-          overrides = let
-            tmfTestSrc = import ./fficxx-multipkg-test/template-member/gen.nix {
-              inherit (pkgs) stdenv;
-              haskellPackages = newHaskellPackages0;
-            };
-            tmplDepTestSrc = import ./fficxx-multipkg-test/template-dep/gen.nix {
-              inherit (pkgs) stdenv;
-              haskellPackages = newHaskellPackages0;
-            };
-            tmplTopLevelTestSrc =
-              import ./fficxx-multipkg-test/template-toplevel/gen.nix {
-                inherit (pkgs) stdenv;
-                haskellPackages = newHaskellPackages0;
-              };
-
-          in self: super:
-          finalHaskellOverlay self super // {
-            "fficxx-test" = self.callCabal2nix "fficxx-test" ./fficxx-test { };
-            "fficxx-multipkg-test" =
-              self.callCabal2nix "fficxx-multipkg-test" ./fficxx-multipkg-test
-              { };
-            "tmf-test" = self.callCabal2nix "tmf-test" tmfTestSrc { };
-            "tmpl-dep-test" =
-              self.callCabal2nix "tmpl-dep-test" tmplDepTestSrc { };
-            "tmpl-dup-inst" = self.callCabal2nix "tmpl-dup-inst"
-              ./fficxx-multipkg-test/tmpl-dup-inst { };
-            "tmpl-toplevel-test" =
-              self.callCabal2nix "tmpl-toplevel-test" tmplTopLevelTestSrc { };
-          };
+        newHaskellPackages = pkgs.haskellPackages.override {
+          overrides = finalHaskellOverlay;
         };
 
       in {
         packages = {
-          "stdcxx-src" = stdcxxSrc;
           inherit (newHaskellPackages)
             fficxx fficxx-runtime stdcxx fficxx-test fficxx-multipkg-test tmf-test
             tmpl-dep-test tmpl-dup-inst tmpl-toplevel-test;
@@ -86,7 +71,7 @@
           });
         };
 
-        devShell = newHaskellPackages0.shellFor {
+        devShell = pkgs.haskellPackages.shellFor {
           packages = ps: [ ps.fficxx ps.fficxx-runtime ];
           buildInputs = [ pkgs.cabal-install pkgs.ormolu ];
           withHoogle = false;
@@ -94,4 +79,3 @@
       }
   );
 }
-
