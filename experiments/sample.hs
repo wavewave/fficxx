f34a2c71114347d2e52af59c67ea1c3b3a293925
Ian-Woo Kim <ianwookim@gmail.com>
2023-08-08 16:24:09 -0700

    format

--- experiments/sample.hs
+++ experiments/sample.hs
@@ -1,13 +1,10 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# OPTIONS_GHC -w #-}
 
---test = do
---  addModFinalizer (addForeignSource LangCxx "\n#include \"test\"")
-
 module MyModule where
 
 data K = K Int
 
 test :: IO ()
-
-foreign import ccall safe "ProxyTestLoader.h Loader_delete" c_loader_delete :: Ptr RawLoader-> IO ()
+test = do
+  addModFinalizer (addForeignSource LangCxx "\n#include \"test\"")
