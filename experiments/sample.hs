3a0be27c55783e70cc313b603abcc1c126fc5d1c
Ian-Woo Kim <ianwookim@gmail.com>
2023-08-09 16:10:48 -0700

    ormolu

--- experiments/sample.hs
+++ experiments/sample.hs
@@ -5,14 +5,12 @@ module MyModule where
 
 data K = K Int
 
-{- test :: IO ()
+test :: IO ()
 test = do
   addModFinalizer (addForeignSource LangCxx "\n#include \"test\"")
-
--}
 
 instance (C a) => D (P a) (Q a) where
   type F (P a) = Double
   dinst x = x * x
 
-newtype Loader = Loader (Ptr RawLoader) deriving (Eq,Ord,Show)
+newtype Loader = Loader (Ptr RawLoader) deriving (Eq, Ord, Show)
